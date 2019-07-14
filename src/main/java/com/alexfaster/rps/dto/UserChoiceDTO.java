aef0758a816f0207436fa4b4b97185ad7c50b616
Alex Faster <4190200@gmail.com>
2019-07-14 15:11:30 +0300

    added more validation

--- src/main/java/com/alexfaster/rps/dto/UserChoiceDTO.java
+++ src/main/java/com/alexfaster/rps/dto/UserChoiceDTO.java
@@ -3,9 +3,12 @@ package com.alexfaster.rps.dto;
 import com.alexfaster.rps.model.Choice;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class UserChoiceDTO {
 
+    @NotNull
     private Choice choice;
 
 }
